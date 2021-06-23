cd0aa36dd1e0203753016e246c0ba2c7132ee904
ZiWei Rao <r3033595562@gmail.com>
2021-06-23 18:46:27 +0800

    For test nginx route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Api from './pages/Api';
+import Test from './pages/Test';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Charlie's Restaurant!</h1>
-    </div>
+    <Router>
+      <Route path="/api" exact component={Api} />
+      <Route path="/test" exact component={Test} />
+    </Router>
   );
 }
 
---
+++ src/pages/Api.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Api() {
+    return (
+        <div>
+            Here is react api page.
+        </div>
+    )
+}

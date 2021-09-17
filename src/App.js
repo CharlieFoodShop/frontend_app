b009e22fff7fb8d7e4f191625dcc4090cc4ca3df
ZiWei Rao <r3033595562@gmail.com>
2021-09-17 00:23:49 -0400

    Add manager index page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import ManagerLogIn from './pages/manager_pages/ManagerLogIn'
 import ManagerRegister from './pages/manager_pages/ManagerRegister';
 import ManagerResetPassword from './pages/manager_pages/ManagerResetPassword';
 import ManagerResetPasswordToken from './pages/manager_pages/ManagerResetPasswordToken';
+import ManagerIndex from './pages/manager_pages/ManagerIndex';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/manager/" exact component={ManagerIndex} />
         <Route path="/manager/login" exact component={ManagerLogIn} />
         <Route path="/manager/register" exact component={ManagerRegister} />
         <Route path="/manager/reset-password/" exact component={ManagerResetPassword} />
--- src/pages/manager_pages/ManagerIndex.js
+++ src/pages/manager_pages/ManagerIndex.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Layout, Menu, Breadcrumb, message, Spin } from 'antd';
+import { UserOutlined, FileImageOutlined } from '@ant-design/icons';
+import { Route } from 'react-router-dom';
+
+import Footer from './components/Footer';
 
 export default () => {
     return (
-        <div>
+        <Layout style={{ minHeight: '100vh' }}>
+            <Spin tip="Loading..." spinning={false}>
+                <Layout.Sider collapsible>
+                    <Menu mode="inline">
 
-        </div>
+                    </Menu>
+                </Layout.Sider>
+                <Layout className="site-layout">
+
+                </Layout>
+            </Spin>
+        </Layout>
     )
 }
